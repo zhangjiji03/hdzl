071428ffc0ab86e24a2502105420a4ef7257d4f8
Your Name <you@example.com>
2020-12-29 10:18:18 +0800

    add

--- src/main/java/com/zz/zzApplication.java
+++ src/main/java/com/zz/zzApplication.java
@@ -3,9 +3,11 @@ package com.zz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableAsync
 @MapperScan("com.zz.mapper")
 @SpringBootApplication
 public class zzApplication {
