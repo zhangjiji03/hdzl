361efeb57b0cb4de0dbb0ed0c586227c4670fc51
Your Name <you@example.com>
2021-05-26 11:03:57 +0800

    update

--- src/main/java/com/aiboatapi/controller/UserController.java
+++ src/main/java/com/aiboatapi/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -20,7 +21,8 @@ import javax.validation.Valid;
  * 版本:         1.0
  */
 @Validated
-@RestController("/user")
+@RequestMapping("/user")
+@RestController
 public class UserController {
 
     @Autowired
